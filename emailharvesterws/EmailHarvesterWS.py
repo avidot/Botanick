37a53a77b7b798c63d5bd8079272d1cd97ea9066
Hervé Beraud <herveberaud.pro@gmail.com>
2016-11-09 13:26:00 +0100

    [refactor] split search function
    Create sub functionnalities from search function

--- emailharvesterws/EmailHarvesterWS.py
+++ emailharvesterws/EmailHarvesterWS.py
@@ -8,6 +8,10 @@ import os
 from flask import Flask
 import glob
 
+app = Flask(__name__)
+engines = ["google", "linkedin", "bing", "yahoo", "github"]
+
+
 class myThread (threading.Thread):
     def __init__(self, domain, engine):
         threading.Thread.__init__(self)
@@ -19,8 +23,25 @@ class myThread (threading.Thread):
 def callEmailHarvester(domain,engine):
     result = subprocess.call('emailharvester -d '+domain+' -e '+engine+' -s result_'+engine+'.txt', shell=True)
     
+def extractFileContent(filename):
+    emails_found=""
+    with open(filename) as f:
+        for line in f:
+            if line not in emails_found:
+                if emails_found == "":
+                    emails_found=line
+                else:
+                    emails_found=emails_found+", "+line
+    return emails_found
+    
+def getResults():
+    list_of_files = glob.glob('./result_*.txt')
+    emails_found=""
+    for file_name in list_of_files:
+        emails_found += extractFileContent(file_name)
+        os.remove(file_name)
+    return emails_found
 
-app = Flask(__name__)
 
 def search2(domain):
     return "test"
@@ -28,8 +49,8 @@ def search2(domain):
 @app.route('/domain=<domain>')
 def search(domain):
     threads = []
+    
     # Setup search engines
-    engines = ["google", "linkedin", "bing", "yahoo", "github"]
     for engine in engines:
         current_thread = myThread(domain, engine)
         current_thread.start()
@@ -38,20 +59,9 @@ def search(domain):
     # Wait for all threads to complete
     for t in threads:
         t.join()
+        
+    return getResults()
     
-    list_of_files = glob.glob('./result_*.txt')
-    emails_found=""
-    for file_name in list_of_files:
-        with open(file_name) as f:
-            for line in f:
-                if line not in emails_found:
-                    if emails_found == "":
-                        emails_found=line
-                    else:
-                        emails_found=emails_found+", "+line
-        os.remove(file_name)
-
-    return emails_found
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
