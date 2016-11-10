2721a3d5c8bfcf3a6945e8744e4887688578ce9f
VIDOT Adrien <adrien974@MacBook-Pro-de-VIDOT.local>
2016-11-10 17:34:54 +0100

    Revert "Revert "Fix a codacy issue""
    This reverts commit 6551c882745b13d5b9be183e83f379e34b067921.

--- tests/test_emailharvesterws.py
+++ tests/test_emailharvesterws.py
@@ -8,12 +8,9 @@ test_botanick
 Tests for `botanick` module.
 """
 
-import pytest
-
 
 from botanick import Botanick
 
 def test_botanick():
 	emails_found = Botanick.search("squad.pro")
 	assert emails_found != ""
-	print(emails_found)
