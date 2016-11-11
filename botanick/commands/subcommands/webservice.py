34f8a2e6b63b637d40002685d7976cdf38751de0
herve beraud <herveberaud.pro@gmail.com>
2016-11-12 00:21:12 +0100

    [feat] add web dashboard reachable at /

--- botanick/commands/subcommands/webservice.py
+++ botanick/commands/subcommands/webservice.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask
 from botanick.core.harvester import harvest
 from botanick.core.converters import tostring
+from botanick.const import BASE_PATH
 
 
 app = Flask("Botanick")
@@ -15,6 +17,12 @@ def search(domain):
         domain -- the domain name
     """
     return tostring(harvest(domain))
+
+
+@app.route('/')
+def index():
+    with open(os.path.join(BASE_PATH, 'botanick', 'templates', 'index.py')) as template:
+        return "".join(template.readlines())
 
 
 def webservice(args):
